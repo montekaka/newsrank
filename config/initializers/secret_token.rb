a68a769400e456e0a7c1c48f525a157a9536f6bc
montekaka <josh.chen.jia@gmail.com>
2014-01-13 19:49:02 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Newsrank::Application.config.secret_key_base = 'e4bbbd25f443865363967b3fcdc1b954b848354aff5375af29724a31b493e786d11aa0de7daf66ae66baf7a90b7435c16b882178164c47d117e53d2689c1c592'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Newsrank::Application.config.session_store :cookie_store, key: '_newsrank_session'
